fab539765dd2b3810517e86fe386025f93b2fb61
Joshua S. Goller <jsgoller1@gmail.com>
2015-06-13 07:16:17 -0700

    forgot netcat xD

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ apt-get upgrade
 apt-get install git tmux vim gcc clang vlc npm ipython nodejs irssi
 
 # Aptitude: install security stuff I use
-apt-get install dsniff hping3 nmap wireshark kismet
+apt-get install dsniff hping3 nmap wireshark kismet netcat
 apt-get autoremove
 
 # Get correct version of PIP
